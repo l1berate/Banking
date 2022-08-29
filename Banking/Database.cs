71b552005accc9df33b70ba5d5e6e69a4ecc4e47
Jacob Wolfe <mr.jdwolfe@gmail.com>
2022-08-28 22:24:22 -0500

    Fleshed out the createAccount method in Database but it still hasn't been tested. Also, the checkLogin and checkAdminLogin methods now close the connection to the database properly.

--- Banking/Database.cs
+++ Banking/Database.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Security.AccessControl;
 using System.Diagnostics;
+using System.Security.Principal;
 
 namespace Banking
 {
@@ -25,7 +26,9 @@ namespace Banking
                 sqlCon);
             cmd.Parameters.AddWithValue("@usrName", usrName);
             cmd.Parameters.AddWithValue("@pWord", pWord);
-            return (Convert.ToInt32(cmd.ExecuteScalar()) == 1) ? true : false;
+            bool returnValue = (Convert.ToInt32(cmd.ExecuteScalar()) == 1) ? true : false;
+            sqlCon.Close();
+            return returnValue;
         }
 
         // check admin login creds
@@ -37,37 +40,80 @@ namespace Banking
                 sqlCon);
             cmd.Parameters.AddWithValue("@usrName", usrName);
             cmd.Parameters.AddWithValue("@pWord", pWord);
-            return (Convert.ToInt32(cmd.ExecuteScalar()) == 1) ? true : false;
+            bool returnValue = (Convert.ToInt32(cmd.ExecuteScalar()) == 1) ? true : false;
+            sqlCon.Close();
+            return returnValue;
         }
 
 
         // create account
-        public bool createAccount(string accName, string userName, string passWord, 
-            string ssn, string email, string phoneno, string accType, double accBalance,
+        public void createAccount(string accName, string userName, string passWord, 
+            string ssn, string email, string phoneno, string[] accType, double[] accBalance,
             bool isAdmin=false)
         {
-            if (!isAdmin)
-            {
-                // 3 insert into commands to generate new account
-                // use try/catch blocks and return false if account creation fails
-                // easy way to pass error message to consolemenu?
-                SqlCommand cmd = new SqlCommand(
-                    "", 
-                    sqlCon);
-            }
-            else
-            {
-                // same as above but admin bit is set to true (1)
+            sqlCon.Open();
+            
+            // 3 insert into commands to generate new account
+            SqlCommand cmd = new SqlCommand(
+                "insert into USERS (accName, accUsername, accPassword, accSSN, accEmail, accPhone, isAdmin) values (@accName, @userName, @passWord, @ssn, @email, @phoneno, @isAdmin)", 
+                sqlCon);
+            cmd.Parameters.AddWithValue("@accName", accName);
+            cmd.Parameters.AddWithValue("@userName", userName);
+            cmd.Parameters.AddWithValue("@passWord", passWord);
+            cmd.Parameters.AddWithValue("@ssn", ssn);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@phoneno", phoneno);
+            cmd.Parameters.AddWithValue("@isAdmin", (isAdmin) ? 1 : 0);
+            cmd.ExecuteNonQuery();
 
+            // get user number of newly created account for ACCOUNTS table
+            SqlCommand cmd1 = new SqlCommand(
+                "select userNumber from USERS where accUsername=@userName and accPassword=@passWord",
+                sqlCon);
+            cmd1.Parameters.AddWithValue("@userName", userName);
+            cmd1.Parameters.AddWithValue("@passWord", passWord);
+            int usrNo = Convert.ToInt32(cmd1.ExecuteScalar());
+
+            // insert info for the ACCOUNTS table now and grab the account numbers
+            
+            foreach (string acct in accType)
+            {
+                // create entry in ACCOUNTS table
+                SqlCommand cmd2 = new SqlCommand(
+                    "insert into ACCOUNTS (userNumber, accType, accBalance) values (@usrNo, @acct, @accBal)",
+                    sqlCon);
+                cmd2.Parameters.AddWithValue("@usrNo", usrNo);
+                cmd2.Parameters.AddWithValue("@acct", acct);
+                cmd2.Parameters.AddWithValue("@accBal", accBalance[0]);
+                cmd2.ExecuteScalar();
+
+                // get account number of newly created account for TRANSACTIONS table
+                SqlCommand cmd3 = new SqlCommand(
+                    "select accNumber from ACCOUNTS where userNumber=@usrNo and accType=@acct",
+                    sqlCon);
+                cmd3.Parameters.AddWithValue("@usrNo", usrNo);
+                cmd3.Parameters.AddWithValue("@acct", acct);
+                int accNo = Convert.ToInt32(cmd3.ExecuteScalar());
+
+                // add entry for 'Initial Deposit' in TRANSACTIONS
+                SqlCommand cmd4 = new SqlCommand(
+                    "insert into TRANSACTIONS (accNumber, userNumber, transAmount, transDescription) values (@accNo, @usrNo, @accBal, @transD)",
+                    sqlCon);
+                cmd4.Parameters.AddWithValue("@accNo", accNo);
+                cmd4.Parameters.AddWithValue("@usrNo", usrNo);
+                cmd4.Parameters.AddWithValue("@accBal", accBalance[0]);
+                cmd4.Parameters.AddWithValue("@transD", (acct == "Loan") ? "Loan Approved" : "Initial Deposit");
+                cmd4.ExecuteScalar();
             }
-            return false;
+            sqlCon.Close();
         }
 
         // delete account
-        public bool deleteAccount(int usrNo)
+        public void deleteAccount(int usrNo)
         {
-
-            return false;
+            sqlCon.Open();
+            
+            sqlCon.Close();
         }
 
         // edit account
